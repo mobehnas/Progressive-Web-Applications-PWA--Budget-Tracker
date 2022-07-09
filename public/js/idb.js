1b2e3220561853d49c01ebfb5dcee14c6123ed24
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-07-09 19:29:48 -0400

    add screenshot

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,16 +1,15 @@
 let db;
-// create a new db request for a "budget" database.
+
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-    // create object store called "pending" and set autoIncrement to true
-    const db = event.target.result;
+    
     db.createObjectStore("pending", {autoIncrement: true});
 };
 
 request.onsuccess = function (event) {
         db = event.target.result;
-        // check if app is online before reading from db
+      
         if (navigator.onLine) {
             checkDatabase();
         }
@@ -34,9 +33,7 @@ function saveRecord(record) {
 function checkDatabase() {
    
     const transaction = db.transaction(["pending"], "readwrite");
-    // access your pending object store
     const store = transaction.objectStore("pending");
-    // get all records from store and set to a variable
     const getAll = store.getAll();
 
     getAll.onsuccess = function () {
@@ -51,18 +48,15 @@ function checkDatabase() {
             })
                 .then(response => response.json())
                 .then(() => {
-                    // if successful, open a transaction on your pending db
+                    
                     const transaction = db.transaction(["pending"], "readwrite");
-
-                    // access your pending object store
                     const store = transaction.objectStore("pending");
 
-                    // clear all items in your store
                     store.clear();
                 });
         }
     };
 }
 
-// listen for app coming back online
+
 window.addEventListener("online", checkDatabase);
